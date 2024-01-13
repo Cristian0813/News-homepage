d8f671747d6fe9a6b8f0c5e52191bb317fe6b73e
Cristian Arias <javier.0813@hotmail.com>
2024-01-13 01:39:30 -0500

    Se cambia el color del texto

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         'SoftOrange': 'hsl(35, 77%, 62%)',
         'SoftRed': 'hsl(5, 85%, 63%)',
         'OffWhite': 'hsl(36, 100%, 99%)',
-        'GrayishBlue': 'hsl(233, 8%, 79%)',
+        'GrayishBlue': 'hsl(233, 8%, 25%)',
         'DarkGrayishBlue': 'hsl(236, 13%, 42%)',
         'VeryDarkBlue': 'hsl(240, 100%, 5%)',
       }
